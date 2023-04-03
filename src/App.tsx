462e8cf85e9128ff8366b31e0f15e58b1996286f
Samir <samir.andrade@ceuma.br>
2023-04-03 18:43:37 -0300

    adicionando themeprovider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
-import logo from './logo.svg'
+import { ThemeProvider } from 'styled-components'
 import './App.css'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <h1>hellooo</h1>
-    </>
+    </ThemeProvider>
   )
 }
