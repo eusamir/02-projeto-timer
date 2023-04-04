e12eb808ebfbb0c42d856efa5663b5363b413128
Samir <samir.andrade@ceuma.br>
2023-04-04 17:29:31 -0300

    adicionando eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import { useState } from 'react'
 import { ThemeProvider } from 'styled-components'
 import './App.css'
 import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './global'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <h1>hellooo</h1>
+
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
